3625d4bde146c2387788986f9e41d0520ee9ae4d
Pablo <psr22052002@gmail.com>
2025-08-24 11:37:00 +0200

    Añadido firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDEWxXIHlD8ezQTdklpvys2FNjFu5bqd2s",
+  authDomain: "checklist-real-murcia.firebaseapp.com",
+  databaseURL: "https://checklist-real-murcia-default-rtdb.firebaseio.com",
+  projectId: "checklist-real-murcia",
+  storageBucket: "checklist-real-murcia.firebasestorage.app",
+  messagingSenderId: "760418626321",
+  appId: "1:760418626321:web:5aa01ddfd53e9be79bbab0",
+  measurementId: "G-DWVT87PSEZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
